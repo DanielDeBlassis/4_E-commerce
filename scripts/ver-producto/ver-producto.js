b84f3299eb503fa8e272ad3d42b4de1d454e9778
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-06 10:56:35 -0300

    Crea función para cargar productos similares
    La misma toma el contenedor y crea una variable con el contenido a agregale al mismo.
    También comprueba qué imagen se seleccionó, para no mostrarla como sugerencia, y luego sortea seis productos en la lista evitando que se repitan. Finalmente los agrega al cotenedor.

--- scripts/ver-producto/ver-producto.js
+++ scripts/ver-producto/ver-producto.js
@@ -36,3 +36,41 @@ function mostrarProducto() {
     }
 
 }
+
+function cargarProductoSimilares() {
+    const $contenedorSimilares = document.querySelector(".productos__similares");
+    let contenido = `<div class="categoria_producto__titulo">
+                    <h2>Productos Similares</h2>
+                    </div>
+                    <div class="categoria_producto__box">`;
+
+    let numerosSalidos = [];
+
+    if (urlImagen != null) {
+        numerosSalidos.push(imagenesProductos.indexOf(`${urlImagen}`));
+    }else {
+        numerosSalidos.push(imagenesProductos.indexOf(`assets/images/productos/star-wars/stormtrooper.svg`));
+    }
+
+    let contador = 0;
+
+    while (contador < 6) {
+
+        let nroAzar = Math.round(Math.random() * 17);
+
+        if (numerosSalidos.indexOf(nroAzar) === -1) {
+            contenido += `<div class="categoria_producto__item">
+            <img class="imagen__producto" src="../${imagenesProductos[nroAzar]}" alt="Imagen Producto" />
+            <h5>Producto XYZ</h5>
+            <p>$ 60.00</p>
+            <a  href="../screens/ver-producto.html" onclick="guardarProducto(${nroAzar + 1})">Ver producto</a>
+            </div>`;
+            numerosSalidos.push(nroAzar);
+            contador++;
+        }
+    }
+
+    contenido += `</div>`;
+    $contenedorSimilares.innerHTML = contenido;
+
+}
