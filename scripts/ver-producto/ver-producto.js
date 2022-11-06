d7c22dcc0b70d5ee739725a84cd331b0bb2ef129
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-06 10:47:23 -0300

    Crea arreglo con todas la imágenes

---
+++ scripts/ver-producto/ver-producto.js
@@ -0,0 +1,21 @@
+
+const imagenesProductos = [
+    "assets/images/productos/star-wars/stormtrooper.svg",
+    "assets/images/productos/star-wars/darth-vader.svg",
+    "assets/images/productos/star-wars/yoda.svg",
+    "assets/images/productos/star-wars/stormtrooper2.svg",
+    "assets/images/productos/star-wars/baby-yoda.svg",
+    "assets/images/productos/star-wars/anonimous.svg",
+    "assets/images/productos/consolas/joystick.svg",
+    "assets/images/productos/consolas/ps5.svg",
+    "assets/images/productos/consolas/ps0.svg",
+    "assets/images/productos/consolas/two-controls.svg",
+    "assets/images/productos/consolas/black-box.svg",
+    "assets/images/productos/consolas/retro.svg",
+    "assets/images/productos/diversos/what1.svg",
+    "assets/images/productos/diversos/what2.svg",
+    "assets/images/productos/diversos/sonic.svg",
+    "assets/images/productos/diversos/what3.svg",
+    "assets/images/productos/diversos/virtual-reality.svg",
+    "assets/images/productos/diversos/pikachu.svg"
+];
