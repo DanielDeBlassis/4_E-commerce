0d62ab002d478ad0ff5e4c355b7fcf8ffd41c182
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-12 18:08:37 -0300

    Crea función para crear producto
    La misma toma los datos del formulario, y los intenta subir a la base de datos creando un producto mediante un bloque try/catch, el cual notificará tanto en caso de éxito como de error mediante una alerta de tipo sweetalert.
    Si el formulario se envía con éxito, se reseteará.

--- controllers/agregar-producto.js
+++ controllers/agregar-producto.js
@@ -2,3 +2,24 @@ import { saveProducto } from "../services/client-service.js";
 
 const formularioAgregar = document.querySelector(".formulario");
 
+formularioAgregar.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const $urlImagen = document.querySelector("input[name='url-img']").value;
+    const $categoria = document.querySelector("input[name='categoria']").value;
+    const $nombre = document.querySelector("input[name='nombre-producto']").value;
+    const $precio = document.querySelector("input[name='precio']").value;
+    const $stock = document.querySelector("input[name='stock']").value;
+    const $enPromocion = document.querySelector("select[name='promocion']").value;
+    const $descripcion = document.querySelector("textarea[name='descripcion']").value;
+
+    try {
+        saveProducto($categoria, $nombre, $descripcion, parseFloat($precio), parseInt($stock), Boolean(parseInt($enPromocion)), $urlImagen);
+        swal("Correcto!", "Producto agregado exitosamente", "success");
+        formularioAgregar.reset();
+    }catch(error){
+        swal("Error!", "Ocurrió un error, vuelve a intentarlo", "error");
+        console.log("Error al agregar el producto: " + error);
+    }
+    
+});
