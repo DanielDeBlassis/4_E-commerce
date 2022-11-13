7a2f567297109c6175693c80b71ba9f5761fb91f
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-13 00:09:17 -0300

    Modifica función para adaptar header
    La misma ahora trabajará con la clase oculto y el estilo display para mostrar u ocultar el buscador según sea el caso.

--- scripts/header/header.js
+++ scripts/header/header.js
@@ -1,9 +1,22 @@
 function adaptar() {
+    const $buscadorAlternativo = document.querySelector(".buscador__alternativo");
+    const $buscadorNormal = document.querySelector(".formulario-busqueda")
+
     if (screen.width <= 450) {
+        $buscadorNormal.style.display = "none";
+        $buscadorNormal.classList.add("oculto");
+        $buscadorAlternativo.style.display = "flex";
+        $buscadorAlternativo.classList.remove("oculto");
     } else if (screen.width > 450) {
+        $buscadorAlternativo.style.display = "none";
+        $buscadorAlternativo.classList.add("oculto");
+        $buscadorNormal.style.display = "flex";
+        $buscadorNormal.classList.remove("oculto");
     }
     adaptarBoton();
 }
+
+
 let opcion = 0;
 
 function mostrarBuscador() {
