408d089926ac95d2ed80c9b1595497e588162e5e
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-13 00:04:47 -0300

    Elimina inyección de código HTML en header
    El mismo estaba generando problemas en el uso desde celulares.

--- scripts/header/header.js
+++ scripts/header/header.js
@@ -1,23 +1,6 @@
-const bloqueMenor = `<i onclick="mostrarBuscador()" class="fas fa-search"></i>
-<div class="container__form">
-        <label>
-          <input class="form-control busqueda" type="search" placeholder="Buscar" />
-        </label>
-        <button class="btn btn-secondary btn-search" type="submit" title="Buscar"><i class="fas fa-search"></i></button>
-    </div>`;
-
-const bloqueMayor = `<label>
-<input class="form-control busqueda" type="search" placeholder="¿Qué deseas buscar?" />
-</label>
-<button class="btn btn-light" type="submit" title="Buscar"><i class="fas fa-search"></i></button>`;
-
-
 function adaptar() {
-    const $formularioBusqueda = document.querySelector(".formulario-busqueda");
     if (screen.width <= 450) {
-        $formularioBusqueda.innerHTML = bloqueMenor;
     } else if (screen.width > 450) {
-        $formularioBusqueda.innerHTML = bloqueMayor;
     }
     adaptarBoton();
 }
