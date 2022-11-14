d24bfbec7ef7a142c1d25125398ba1dae4805b1e
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 19:37:01 -0300

    Obtiene contenedor de sugerencias

--- scripts/header/buscador.js
+++ scripts/header/buscador.js
@@ -1,3 +1,4 @@
 import { onGetProductos } from "../../services/client-service.js";
 
 const $inputBusqueda = document.querySelector(".busqueda");
+const $sugerencias = document.querySelector(".sugerencias");
