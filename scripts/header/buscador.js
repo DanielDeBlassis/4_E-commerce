3a604994109c39b1c57216edfcb170bd48766e1a
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 19:41:07 -0300

    Crea función para obtener todos los productos
    Apenas termina de cargar la página, comienza a obtener todos los productos junto con su id de Firestore, y los guarda en el arreglo previamente creado para ello.
    Además se oculta el contenedor de sugerencias.

--- scripts/header/buscador.js
+++ scripts/header/buscador.js
@@ -3,3 +3,14 @@ import { onGetProductos } from "../../services/client-service.js";
 const $inputBusqueda = document.querySelector(".busqueda");
 const $sugerencias = document.querySelector(".sugerencias");
 let todosLosProductos = [];
+
+window.addEventListener("DOMContentLoaded", () => {
+    onGetProductos((productos) => {
+        productos.forEach(doc => {
+            const producto = doc.data();
+            const id = doc.id;
+            todosLosProductos.push({ id: id, producto: producto });
+        });
+    });
+    $sugerencias.style.display = "none";
+});
