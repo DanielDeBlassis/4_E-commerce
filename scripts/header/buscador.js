0961a9e9099a8e9dabdffc252916e4c59e4790da
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 19:38:39 -0300

    Crea array para guardar todos los productos

--- scripts/header/buscador.js
+++ scripts/header/buscador.js
@@ -2,3 +2,4 @@ import { onGetProductos } from "../../services/client-service.js";
 
 const $inputBusqueda = document.querySelector(".busqueda");
 const $sugerencias = document.querySelector(".sugerencias");
+let todosLosProductos = [];
