79a2affd30b23e97a671b7d5e34f52ea3ad13cf3
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 21:14:47 -0300

    Obtiene campos del formulario

--- controllers/editar-usuario.js
+++ controllers/editar-usuario.js
@@ -3,3 +3,8 @@ import { getUsuario, updateUsuario } from "../services/client-service.js";
 const url = new URL(window.location);
 let id = url.searchParams.get("id");
 
+let $nombre = document.querySelector("input[name='nombreUsuario']");
+let $mail = document.querySelector("input[name='email']");
+let $clave = document.querySelector("input[name='password']");
+let $claveRepetida = document.querySelector("input[name='password-repeat']");
+
