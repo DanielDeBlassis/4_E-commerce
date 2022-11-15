b57dee9a3c48720c7446ba073c37783aea35181f
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 21:44:59 -0300

    Obtiene input check para mostrar la contraseña

--- controllers/crear-usuario.js
+++ controllers/crear-usuario.js
@@ -1,3 +1,4 @@
 import { saveUsuario } from "../services/client-service.js";
 
 const formularioCrear = document.querySelector(".formulario");
+const $mostrarClave = document.querySelector("input[name='mostrar']");
