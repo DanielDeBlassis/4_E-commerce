8626ab68840b9877b6800d5f64cdae5b7fd711e6
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 21:46:33 -0300

    Crea función para mostrar u ocultar la contraseña
    La misma escucha el evento click del input check y comprueba el estado del mismo. Si está checkeado, cambia el tipo de input correspondiente a la contraseña a texto; de lo contrario se vuelve a establecer como de tipo password, ocultando así la contraseña.

--- controllers/crear-usuario.js
+++ controllers/crear-usuario.js
@@ -2,3 +2,15 @@ import { saveUsuario } from "../services/client-service.js";
 
 const formularioCrear = document.querySelector(".formulario");
 const $mostrarClave = document.querySelector("input[name='mostrar']");
+
+$mostrarClave.addEventListener("click", () => {
+    const $clave = document.querySelector("input[name='password']");
+    const $claveRepetida = document.querySelector("input[name='password-repeat']");
+    if ($mostrarClave.checked) {
+        $clave.setAttribute("type", "text");
+        $claveRepetida.setAttribute("type", "text");
+    }else {
+        $clave.setAttribute("type", "password");
+        $claveRepetida.setAttribute("type", "password");
+    }
+});
