d8457c5c3688e5add4721338ec0a17af3359182f
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 21:26:40 -0300

    Crea función para cargar datos en el formulario
    Se obtienen los datos del usuario mediante el ID y se cargan en el formulario.

--- controllers/editar-usuario.js
+++ controllers/editar-usuario.js
@@ -8,3 +8,17 @@ let $mail = document.querySelector("input[name='email']");
 let $clave = document.querySelector("input[name='password']");
 let $claveRepetida = document.querySelector("input[name='password-repeat']");
 
+window.addEventListener("DOMContentLoaded", async () => {
+
+    const doc = await getUsuario(id);
+    const usuario = doc.data();
+
+
+    $nombre.value = usuario.nombre;
+    $mail.value = usuario.mail;
+    $clave.value = usuario.clave;
+    $claveRepetida.value = usuario.clave;
+
+
+});
+
