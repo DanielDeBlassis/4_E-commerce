46b1557c4d51189429226793de593a025757e708
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 22:00:29 -0300

    Crea función para enviar el mensaje al correo
    Mediante emailjs se envía el mensaje introducido al correo. Primero se previene el comportamiento por defecto del evento y luego se cambia el texto del botón indicando que  el mensaje se está enviando.
    Luego se declaran dos variables con el ID tanto del servicio como de la plantilla a utilizar.
    Finalmente se intenta enviar el mensaje y en tal caso emergerá una sweetalert indicando que fue un éxito y se reseteará el formulario.
    Si ocurre un error una sweetalert lo notificará y se imprimirá el error por consola.

--- scripts/footer/contacto.js
+++ scripts/footer/contacto.js
@@ -3,3 +3,22 @@ const formulario = document.getElementById("form");
 
 emailjs.init('KOXja774hIl3M7vPy')
 
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    btn.textContent = "Enviando...";
+
+    const serviceID = 'service_baubkgr';
+    const templateID = 'template_o8p9wzu';
+
+    // emailjs.sendForm(serviceID, templateID, this)
+    //   .then(() => {
+        // btn.textContent =  'Enviar mensaje';
+    //     swal("Mensaje enviado exitosamente!", "Muchas Gracias!", "success");
+    //     formulario.reset();
+    //   }, (err) => {
+    //     btn.value = 'Enviar mensaje';
+    //     swal("Ocurrió un error!", "Vuelve a intentarlo", "error");
+    //     console.log(JSON.stringify(err));
+    //   });
+  });
