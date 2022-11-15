66adb1e46a134859436efe7f47219d95a683ec08
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-14 21:49:49 -0300

    Crea función para guardar nuevo usuario en Firestore
    Primero se previene el comportamiento por defecto del formulario al ser enviado, evitando que se recargue la página.
    Luego se obtiene el valor de todos los campos y mediante un bloque try/catch se llama a la función para guardar en Firestore; en caso de que todo salga bien se emitirá una sweetalert indicándo el éxito en la operación y luego se reseteará el formulario.
    Si hay un error el mismo será capturado y se emitirá una sweetalert indicándonos que algo salió mal y luego por consola se imprimirá el error.

--- controllers/crear-usuario.js
+++ controllers/crear-usuario.js
@@ -14,3 +14,22 @@ $mostrarClave.addEventListener("click", () => {
         $claveRepetida.setAttribute("type", "password");
     }
 });
+
+formularioCrear.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const $nombre = document.querySelector("input[name='nombreUsuario']").value;
+    const $mail = document.querySelector("input[name='email']").value;
+    const $clave = document.querySelector("input[name='password']").value;
+    const $claveRepetida = document.querySelector("input[name='password-repeat']").value;
+
+    try {
+        saveUsuario($nombre, $clave, $mail);
+        swal("Correcto!", "Usuario creado exitosamente", "success");
+        formularioCrear.reset();
+    } catch (error) {
+        swal("Error!", "Ocurrió un error, vuelve a intentarlo", "error");
+        console.log("Error al crear nuevo usuario: " + error);
+    }
+
+});
