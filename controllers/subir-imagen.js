da10b5b5717618dfd51b15218adb7c1255c2c8e9
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-16 17:15:01 -0300

    Crea función para subir la imagen a firebase
    Se escucha el evento change del input donde se selecciona el archivo y de manera asíncrona se captura el evento, guardando el archivo seleccionado en la posición 0 de la lista de archivos.
    Si el archivo seleccionado se corresponde a una imagen entre 9kb y 2mb, generará un nombre único con la función date now, creará una referencia al mismo, mostrará una pantalla de carga y comenzará a subir el archivo a firebase.
    En caso de que todo salga bien, obtendrá un enlace de descarga del archivo recién subido, quitará la pantalla de carga, insertará el enlace en el input donde va la URL de la imagen (el cual está deshabilitado para el usuario), también insertará el nombre de la imagen en un input oculto en el formulario, y finalmente desplegará una sweetalert avisando que todo salió bien.
    Si algo sale mal, quitará la pantalla de carga, aletará al usuario de que ocurrió un error y por consola se imprimirá el mismo.
    En caso de que el usuario elija un archivo que no cumple las condiciones se le alertará y aclararán las condiciones que debe cumplir la imagen.

--- controllers/subir-imagen.js
+++ controllers/subir-imagen.js
@@ -18,3 +18,46 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage();
+const $imagenProducto = document.querySelector('input[name="imagenProducto"]');
+let enlace = "";
+
+const $inputURL = document.querySelector('input[name="url-img"]');
+const $nombreImagen = document.querySelector("input[name='nombre-img']");
+
+$imagenProducto.addEventListener("change", async (e) => {
+
+    const file = e.target.files[0];
+
+    if ((file.size > 9000 && file.size < 2097152 && file.type === "image/png")
+        || (file.size > 9000 && file.size < 2097152 && file.type === "image/jpg")
+        || (file.size > 9000 && file.size < 2097152 && file.type === "image/jpeg")
+        || (file.size > 9000 && file.size < 2097152 && file.type === "image/svg")
+        || (file.size > 9000 && file.size < 2097152 && file.type === "image/svg+xml")) {
+
+        const cleanNombre = Date.now() + (file.name.replace(/\s+/g, "").trim());//quito espacios del nombre del archivo
+        const nombreImg = cleanNombre;
+
+        const storageRef = ref(storage, `images/productos/${nombreImg}`);
+        
+        const $cargaDialogo = document.querySelector(".carga__dialog");
+        $cargaDialogo.classList.remove("oculto");
+
+        uploadBytes(storageRef, file).then(async snapshot => {
+            enlace = await getDownloadURL(storageRef);
+            $cargaDialogo.classList.add("oculto");
+            $inputURL.value = enlace;
+            $nombreImagen.value = nombreImg;
+            swal("Correcto!", "La imagen se subió correctamente", "success");
+            // console.log(snapshot);
+            // console.log(enlace);
+        }).catch(error => {
+            $cargaDialogo.classList.add("oculto");
+            swal("Error!", "Error al subir la imagen a Firebase", "error");
+            console.log("Error al subir la imagen: " + error);
+        });
+
+    } else {
+        swal("Error!", "Ingrese una imagen entre 9kb y 2mb con formato .png/.jpg./.jpeg/.svg", "error");
+    }
+});
+
