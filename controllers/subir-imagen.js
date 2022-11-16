8197a71371bd6805a15f54d4427d6d82274ae73a
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-16 17:05:09 -0300

    Establece conexión con firebase
    Ante el fallo al exportar la función desde donde también está la que corresponde al borrado de la imagen, se rehace la conexión aquí, repitiendo código, quizás innecesariamente.

--- controllers/subir-imagen.js
+++ controllers/subir-imagen.js
@@ -3,3 +3,18 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebas
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBfNGwN-6zRWfX6Co-_a51QunkYibG-_KE",
+    authDomain: "ecommerce-alurageek.firebaseapp.com",
+    databaseURL: "https://ecommerce-alurageek-default-rtdb.firebaseio.com",
+    projectId: "ecommerce-alurageek",
+    storageBucket: "ecommerce-alurageek.appspot.com",
+    messagingSenderId: "145141151933",
+    appId: "1:145141151933:web:08d55db722e5ddaaea9fd4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage();
