3706c44933c4748d9246cc03de2fdb95ad7624a5
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-20 20:17:23 -0300

    Crea función para crear un nuevo usuario
    Similar a la función para registrar un usuario, con la diferencia de que será accesible solo para el administrador, quien podrá realizar un alta de usuario.

--- controllers/crear-usuario.js
+++ controllers/crear-usuario.js
@@ -1,6 +1,8 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { auth } from "../services/auth-service.js"
 import { saveUsuario } from "../services/firestore-service.js"
 
-const formularioCrear = document.querySelector(".formulario");
+const $formularioCrear = document.querySelector(".formulario");
 const $mostrarClave = document.querySelector("input[name='mostrar']");
 
 $mostrarClave.addEventListener("click", () => {
@@ -15,21 +17,38 @@ $mostrarClave.addEventListener("click", () => {
     }
 });
 
-formularioCrear.addEventListener("submit", (e) => {
+//escucha evento envío de datos
+$formularioCrear.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const $nombre = document.querySelector("input[name='nombreUsuario']").value;
-    const $mail = document.querySelector("input[name='email']").value;
-    const $clave = document.querySelector("input[name='password']").value;
-    const $claveRepetida = document.querySelector("input[name='password-repeat']").value;
+    //obtengo los datos
+    const $signupNombre = document.querySelector("input[name='nombreUsuario']");
+    const $signupEmail = document.querySelector("input[name='email']");
+    const $signupClave = document.querySelector("input[name='password']");
+
+    // console.log($signupClave, $signupEmail);
 
     try {
-        saveUsuario($nombre, $clave, $mail);
-        swal("Correcto!", "Usuario creado exitosamente", "success");
-        formularioCrear.reset();
+        const userCredentials = await createUserWithEmailAndPassword(auth, $signupEmail.value, $signupClave.value);
+        const id = userCredentials.user.uid;
+        // console.log(id);
+        saveUsuario(id, $signupNombre.value, $signupEmail.value, "usuario");
+        $formularioCrear.reset();
+        swal("Correcto", "Usuario Creado exitosamente!", "success");
     } catch (error) {
-        swal("Error!", "Ocurrió un error, vuelve a intentarlo", "error");
-        console.log("Error al crear nuevo usuario: " + error);
-    }
+        if (error.code === "auth/email-already-in-use") {
+            swal("Error", "Correo ya registrado", "error");
+        } else if (error.code === "auth/invalid-email") {
+            $signupEmail.style.border = "solid 1px red";
+            document.querySelector(".error-message.email").innerText = "E-mail inválido"
+        } else if (error.code === "auth/weak-password") {
+            $signupClave.style.border = "solid 1px red";
+            document.querySelector(".error-message.clave").innerText = "Contraseña inválida. Mínimo 6 caracteres."
+        } else {
+            swal("Error", "Ocurrió un error. Vuelve a intentar.", "error");
+            console.log(error);
+            console.log(error.code);
+        }
 
-});
+    }
+})
