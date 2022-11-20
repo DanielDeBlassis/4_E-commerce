3c0bf0d4023fb307812cdd73d2b0665e428202ff
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-20 20:05:36 -0300

    Elimina botones de edición y borrado de usuarios
    La edición o eliminación de un usuario se hará desde la consola de firebase, ya que al implementar su servicio autenticación, no se puede editar un usuario desde la aplicación.

--- scripts/gestion-usuarios/gestion-usuarios.js
+++ scripts/gestion-usuarios/gestion-usuarios.js
@@ -9,13 +9,7 @@ window.addEventListener("DOMContentLoaded", async () => {
         usuarios.forEach(user => {
             const usuario = user.data();
             const fila = document.createElement("tr");
-            fila.innerHTML = `<td>${usuario.nombre}</td><td>${usuario.mail}</td><td>${usuario.clave}</td>
-            <td>
-            <div class="btn-edicion-usuario">
-            <a href="../screens/editar-usuario.html?id=${user.id}" title="Editar Usuario"><i class="fas fa-pen btn-editar"></i></a>
-            <a href="#" title="Borrar Usuario"><i class="fas fa-trash btn-borrar" data-id="${user.id}"></i></a>
-            </div>
-            </td>`;
+            fila.innerHTML = `<td>${usuario.nombre}</td><td>${usuario.mail}</td><td>${usuario.rol}</td>`; 
             $cuerpoTabla.appendChild(fila);
         });
 
