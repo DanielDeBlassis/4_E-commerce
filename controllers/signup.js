c963fc15c572f55ae5539148067191888f26852c
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-20 20:08:30 -0300

    Crea función para mostrar contraseña
    La misma se activará si el usuario hace click en la opción para mostrar la contraseña.

--- controllers/signup.js
+++ controllers/signup.js
@@ -44,3 +44,16 @@ $signupForm.addEventListener("submit", async (e) => {
     }
 })
 
+const $mostrarClave = document.querySelector("input[name='mostrar']");
+
+$mostrarClave.addEventListener("click", () => {
+    const $clave = document.querySelector("input[name='password']");
+    const $claveRepetida = document.querySelector("input[name='password-repeat']");
+    if ($mostrarClave.checked) {
+        $clave.setAttribute("type", "text");
+        $claveRepetida.setAttribute("type", "text");
+    }else {
+        $clave.setAttribute("type", "password");
+        $claveRepetida.setAttribute("type", "password");
+    }
+});
