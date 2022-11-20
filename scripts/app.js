14483c85470b5c609aeebdf766394ab797ee9aaf
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-20 20:02:26 -0300

    Crea función para verificar estado sesión usuario
    La misma verifica si hay un usuario activo, y en tal caso ejecuta la función para mostrar u ocultar el botón de login y/o logout. Por ahora esta función solo surte efecto en la página principal.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { auth } from "../services/auth-service.js"
+// import "../controllers/signup.js"
+import "../controllers/logout.js"
+import { loginCheck } from "../scripts/login/loginCheck.js"
+
+onAuthStateChanged(auth, async (user) => {
+    // console.log(user);
+
+    loginCheck(user);
+    
+    // if(user) {
+    //     loginCheck(user);
+    // }else {
+    // }
 });
+
